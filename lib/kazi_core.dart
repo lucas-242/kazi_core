a0b3e22529cc75104362d8ff4bc374fddd8d12b7
Lucas Guimarães <guimaraeslucas242@gmail.com>
2024-09-23 21:35:27 -0300

    feat: Added users module

--- lib/kazi_core.dart
+++ lib/kazi_core.dart
@@ -8,9 +8,6 @@ export 'application/use_cases/users/delete_user_use_case.dart';
 export 'application/use_cases/users/get_user_use_case.dart';
 export 'application/use_cases/users/get_users_use_case.dart';
 export 'application/use_cases/users/update_user_use_case.dart';
-//Data
-export 'data/models/api_response.dart';
-export 'data/repositories/api_users_repository.dart';
 //Domain
 export 'domain/entities/address.dart';
 export 'domain/entities/service.dart';
@@ -53,6 +50,7 @@ export 'presenter/components/svg/kazi_svg.dart';
 export 'presenter/components/webview/kazi_webview.dart';
 export 'shared/extensions/extensions.dart';
 //Shared
+export 'shared/injection/modules/users_module.dart';
 export 'shared/injection/service_injector.dart';
 export 'shared/injection/service_locator.dart';
 export 'shared/l10n/generated/l10n.dart';
