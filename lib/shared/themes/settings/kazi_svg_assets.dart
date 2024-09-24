ea94d83e56e1dd0331e5fa0830282e474e5b8d32
Lucas Guimarães <guimaraeslucas242@gmail.com>
2024-09-23 21:51:55 -0300

    fix: Fixed images export

--- lib/shared/themes/settings/kazi_svg_assets.dart
+++ lib/shared/themes/settings/kazi_svg_assets.dart
@@ -1,12 +1,12 @@
 abstract class KaziSvgAssets {
-  static const String calculator = 'lib/assets/svgs/calculator.svg';
-  static const String email = 'lib/assets/svgs/email.svg';
-  static const String fire = 'lib/assets/svgs/fire.svg';
-  static const String google = 'lib/assets/svgs/google.svg';
-  static const String home = 'lib/assets/svgs/home.svg';
-  static const String logoExtended = 'lib/assets/svgs/logo_extended.svg';
-  static const String logo = 'lib/assets/svgs/logo.svg';
-  static const String person = 'lib/assets/svgs/person.svg';
-  static const String rocket = 'lib/assets/svgs/rocket.svg';
-  static const String services = 'lib/assets/svgs/services.svg';
+  static const String calculator = 'lib/shared/assets/svgs/calculator.svg';
+  static const String email = 'lib/shared/assets/svgs/email.svg';
+  static const String fire = 'lib/shared/assets/svgs/fire.svg';
+  static const String google = 'lib/shared/assets/svgs/google.svg';
+  static const String home = 'lib/shared/assets/svgs/home.svg';
+  static const String logoExtended = 'lib/shared/assets/svgs/logo_extended.svg';
+  static const String logo = 'lib/shared/assets/svgs/logo.svg';
+  static const String person = 'lib/shared/assets/svgs/person.svg';
+  static const String rocket = 'lib/shared/assets/svgs/rocket.svg';
+  static const String services = 'lib/shared/assets/svgs/services.svg';
 }
